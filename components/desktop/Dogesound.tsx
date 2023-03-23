225a7e918b63dca66dee09e79efda60ea8b86e77
smlee <seongmooleekr@gmail.com>
2023-03-23 20:44:59 +0900

    nextconfig edit for caver-js

--- components/desktop/Dogesound.tsx
+++ components/desktop/Dogesound.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import Caver from 'caver-js';
 
 const Dogesound = () => {
     const [isData, setData] = useState('');
     const [isMix, setMix] = useState('');
+    const [isCaver, setCaver] = useState('');
 
     async function getDogesound(){
         const res = await fetch("https://api.dogesound.club/dogesoundwinner")
@@ -13,18 +15,24 @@ const Dogesound = () => {
         const res = await fetch("https://api.dogesound.club/mix/price")
         const data = await res.json()
         setMix(data)
+    }
 
+    async function getCaver(){
+        const caver = new Caver("wss://public-en-cypress.klaytn.net/ws");
+        const res = await caver.rpc.klay.getClientVersion()
+        setCaver(res)
     }
 
     useEffect(()=>{
         getDogesound()
         getMixprice()
+        getCaver()
     },[])
 
     return (
         <>
             <div>
-                {isData} 믹스 금액: {isMix}
+                {isData} 믹스 금액: {isMix} {isCaver}
             </div>
             <div style={{display: 'flex', justifyContent: 'space-around'}}>
                 <div><img src="/banner1.png" style={{width: "100%"}}/></div>
