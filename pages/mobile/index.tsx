df97d0a9db4124a3af0f4e21884ecb2bd12d1979
seongmoolee <seongmooleekr@gmail.com>
2023-03-26 00:51:59 +0900

    text edit

--- pages/mobile/index.tsx
+++ pages/mobile/index.tsx
@@ -34,9 +34,9 @@ const MobileHome = () => {
       <br/>
             Welcome to the Doge Sound Club (DSC), a friendly community that embraces creativity and collaboration through the use of generative Non-Fungible Tokens (NFTs) and an open-license approach.
             <br/><br/>
-            Doge Sound Club is centered around the concept of "Doge Sound," which playfully refers to nonsensical jokes and sayings, known as "Gaesori" in Korean. The term "Doge Sound" derives from the Korean words "Gae" (meaning dog) and "Sori" (meaning sound), signifying lighthearted, entertaining conversations.
+            Doge Sound Club is centered around the concept of &quot;Doge Sound,&quot; which playfully refers to nonsensical jokes and sayings, known as &quot;Gaesori&quot; in Korean. The term &quot;Doge Sound&quot; derives from the Korean words &quot;Gae&quot; (meaning dog) and &quot;Sori&quot; (meaning sound), signifying lighthearted, entertaining conversations.
             <br/><br/>
-            Our project started in July 2021 with the launch of Korea's first generative NFTs called Doge Sound Club Mates. These unique digital collectibles form the basis for our membership activities and foster a sense of belonging within the community.
+            Our project started in July 2021 with the launch of Korea&apos;s first generative NFTs called Doge Sound Club Mates. These unique digital collectibles form the basis for our membership activities and foster a sense of belonging within the community.
             <br/><br/>
             DSC is committed to fostering a creative environment where everyone can contribute, regardless of their background or skill set. We believe in the importance of an open-license approach, ensuring that our stories and content are available for anyone to use and build upon without restrictions.
             <br/><br/>
@@ -49,11 +49,11 @@ const MobileHome = () => {
             <br/><br/>
             At Doge Sound Club (DSC), we also develop Toy Decentralized Applications (Toy DApps) to make blockchain technology more accessible and enjoyable for everyone, regardless of their knowledge about the blockchain. These playful dApps may not have specific utility, but they provide a fun, engaging experience for users.
             <br/><br/>
-            One of our popular Toy DApps is the Doge Sound Contest. This competition combines blockchain technology with a Discord bot to host a monthly contest where participants submit their best "Doge Sound" entries. Doge Sound refers to nonsensical jokes and sayings, known as "Gaesori" in Korean. DSC community members vote for the most illogical and unpredictable submissions to select a winner.
+            One of our popular Toy DApps is the Doge Sound Contest. This competition combines blockchain technology with a Discord bot to host a monthly contest where participants submit their best &quot;Doge Sound&quot; entries. Doge Sound refers to nonsensical jokes and sayings, known as &quot;Gaesori&quot; in Korean. DSC community members vote for the most illogical and unpredictable submissions to select a winner.
             <br/><br/>
             The winning Doge Sound is then showcased on the main page of our website. Winners also have the opportunity to mint their Doge Sound as an NFT by clicking a button connected to the smart contract.
             <br/><br/>
-            The idea behind hosting this unique contest is rooted in our project's fiction. The DSC community believes that the world is a simulation, and that inconsistent or unpredictable sayings and actions often cause bugs in the simulation. These bugs serve as clues that the world is a simulation. To encourage the discovery and exploration of these bugs, we've been running the Doge Sound Contest on the Klaytn blockchain since 2021.
+            The idea behind hosting this unique contest is rooted in our project&apos;s fiction. The DSC community believes that the world is a simulation, and that inconsistent or unpredictable sayings and actions often cause bugs in the simulation. These bugs serve as clues that the world is a simulation. To encourage the discovery and exploration of these bugs, we&apos;ve been running the Doge Sound Contest on the Klaytn blockchain since 2021.
             <br/><br/>
             By developing and promoting Toy DApps like the Doge Sound Contest, DSC aims to create a fun and inclusive environment for users to engage with blockchain technology, fostering curiosity and creativity within our community.
             <br/><br/>
