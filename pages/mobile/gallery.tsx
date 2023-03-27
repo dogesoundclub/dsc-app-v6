f48cea1affa83e3a4b866c33dd7cb6cf3dfb63a1
seongmoolee <seongmooleekr@gmail.com>
2023-03-27 17:17:00 +0900

    mobile gallery add

--- pages/mobile/gallery.tsx
+++ pages/mobile/gallery.tsx
@@ -1,29 +1,38 @@
 import React from 'react';
 import styles from '@/styles/mobile/Gallery.module.css';
+import MobileNav from "../../components/mobile/MobileNav";
+import Dogesound from "../../components/mobile/Dogesound";
 
 const Ready = () => {
   return (
         <div style={{maxWidth: "425px", margin: "0 auto", background: "#D9D9D9"}}>
-            <button>Filter</button>
-            <input type="text" placeholder='Search...'/>
-            <button>Go</button>
-            <div className={styles.mates} style={{display: "flex", width: "100%"}}>
-                  <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
-                  <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
-                  <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
-                  <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
+            <div>
+                <img src="dogesoundclub_mobile.png" style={{width: "100%"}}/>
             </div>
-            <div className={styles.mates} style={{display: "flex", width: "100%"}}>
-                <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
-                <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
-                <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
-                <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
-            </div>
-            <div className={styles.mates} style={{display: "flex", width: "100%"}}>
-                <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
-                <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
-                <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
-                <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
+            <Dogesound></Dogesound>
+            <MobileNav></MobileNav>
+            <div style={{textAlign: "center"}}>
+                <button>Filter</button>
+                <input type="text" placeholder='Search...'/>
+                <button>Go</button>
+                <div className={styles.mates} style={{display: "flex", width: "100%", justifyContent: "center"}}>
+                    <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
+                    <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
+                    <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
+                    <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
+                </div>
+                <div className={styles.mates} style={{display: "flex", width: "100%", justifyContent: "center"}}>
+                    <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
+                    <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
+                    <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
+                    <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
+                </div>
+                <div className={styles.mates} style={{display: "flex", width: "100%", justifyContent: "center"}}>
+                    <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
+                    <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
+                    <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
+                    <div><img src="https://storage.googleapis.com/dsc-mate/336/dscMate-0.png" alt="" />Mate #000</div>
+                </div>
             </div>
         </div>
   );
