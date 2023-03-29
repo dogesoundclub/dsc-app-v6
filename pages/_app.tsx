50cb1803799541dc295ed69a4bb15bcdc832c5e2
seongmoolee <seongmooleekr@gmail.com>
2023-03-29 11:26:56 +0900

    pages update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Audiowide } from 'next/font/google'
+import { createGlobalStyle } from 'styled-components'
 
+const GlobalStyle = createGlobalStyle`
+  html, body, #__next, main {
+    height: 100%;
+    width: 100%;
+    overflow: hidden;
+  }
+`;
 
 const audiowide = Audiowide({ 
   weight: '400',
@@ -10,8 +18,11 @@ const audiowide = Audiowide({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <>
+    <GlobalStyle/>
     <main className={audiowide.className}>
       <Component {...pageProps} />
     </main>
+    </>
   )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="description" content="dogesoundclub"/>
+        <meta name="keywords" content="dogesoundclub" />
+      </Head>
       <body>
         <Main />
         <NextScript />
