f6e0d20f8bdd92f1b5c52799a6cb61918c46b25b
seongmoolee <seongmooleekr@gmail.com>
2023-03-30 11:14:13 +0900

    사용하지않는 임포트모듈 삭제

--- components/desktop/DesktopNav.tsx
+++ components/desktop/DesktopNav.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from '@/styles/desktop/DesktopNav.module.css';
-import Image from 'next/image';
 import { useState, useRef } from 'react'
 
 // const navigation = [
