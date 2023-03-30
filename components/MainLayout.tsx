eaca5c2fdb7cdefe4c0ae12644d6dccfeb2125d7
seongmoolee <seongmooleekr@gmail.com>
2023-03-30 11:06:19 +0900

    불필요한 코드 삭제

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -1,20 +1,5 @@
-// import React from "react";
 import DesktopNav from "./desktop/DesktopNav";
 
-// interface MainLayoutProps {
-//   children: React.ReactNode;
-// }
-
-// const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
-//   return (
-//     <>
-//       <DesktopNav />
-//       {children}
-//     </>
-//   );
-// };
-
-// export default MainLayout;
 type LayoutProps = {
   user?: any
   loading?: boolean
