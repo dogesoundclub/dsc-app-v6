832e38494ba4849573eb787fab4a65ab8314575d
seongmoolee <seongmooleekr@gmail.com>
2023-04-12 14:32:23 +0900

    딥링크 삭제

--- components/Wallet/mobileklip.tsx
+++ components/Wallet/mobileklip.tsx
@@ -1,6 +1,6 @@
 export async function connect(): Promise<any> {
     const klipSDK = require("klip-sdk");
-    const res = await klipSDK.prepare.auth({ bappName: "DogeSoundClub", successLink: "https://dogesound.club/activities", failLink: "https://dogesound.club/activities" });
+    const res = await klipSDK.prepare.auth({ bappName: "DogeSoundClub" });
     window.open(`https://klipwallet.com/?target=/a2a?request_key=${res.request_key}`);
     const interval = setInterval(async () => {
         const result = await klipSDK.getResult(res.request_key);
